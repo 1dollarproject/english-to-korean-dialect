a9aaa268c0c8c04a52998e8b15c3b156d1c3d396
hano <hano.dev.kr@gmail.com>
2024-11-26 00:05:36 +0900

    Fix Export option

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  images: {
-    unoptimized: true,
-  },
+  output: 'export',
 };
 
 export default nextConfig;
